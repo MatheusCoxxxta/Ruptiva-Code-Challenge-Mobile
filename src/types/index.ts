91ef079a8fb92091daf4ce1ed6db810c89007cf5
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-05 05:32:05 -0300

    defining some interfaces to be used on project

---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+/**
+ *  Data Types
+ * @param {string} _id
+ * @param {string} name
+ * @param {string} document
+ * @param {string} type
+ */
+
+export interface User {
+  _id?: string,
+  name: string,
+  document: string,
+  type: string
+}
+
+export interface IUsers {
+  _id?: string,
+  name: string,
+  document: string,
+  type: string
+}
