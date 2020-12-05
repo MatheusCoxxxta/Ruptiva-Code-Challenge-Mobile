e3de544bd51c3e241c0a2ea24e6dfcc045f5617b
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-05 05:33:20 -0300

    creating tool to format documents

---
+++ src/tools/documentMasks.ts
@@ -0,0 +1,20 @@
+const cpfMask = (value: string) => {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
+};
+
+const cnpjMask = (value: string) => {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})/, "$1/$2")
+    .replace(/(\d{4})(\d{1,2})/, "$1-$2");
+};
+
+
+export {cpfMask, cnpjMask}
