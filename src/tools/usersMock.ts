be793fd01131089d3bb4e85b7138dbd8d26786a4
Matheus Costa <matheus271200.ms@gmail.com>
2020-12-05 05:34:43 -0300

    array of mocked data to test app

---
+++ src/tools/usersMock.ts
@@ -0,0 +1,64 @@
+let users: { id: number; name: string; document: string; type: string; }[] = [
+  {
+    id: 1,
+    name: "Matheus",
+    document: "49818286839",
+    type: "individual"
+  },
+  {
+    id: 2,
+    name: "Matheus",
+    document: "37399356000150",
+    type: "business"
+  },
+  {
+    id: 3,
+    name: "Matheus",
+    document: "49818286839",
+    type: "individual"
+  },
+  {
+    id: 4,
+    name: "Matheus",
+    document: "37399356000150",
+    type: "business"
+  },
+  {
+    id: 5,
+    name: "Matheus",
+    document: "49818286839",
+    type: "individual"
+  },
+  {
+    id: 6,
+    name: "Matheus",
+    document: "37399356000150",
+    type: "business"
+  },
+  {
+    id: 7,
+    name: "Matheus",
+    document: "49818286839",
+    type: "individual"
+  },
+  {
+    id: 8,
+    name: "Matheus",
+    document: "37399356000150",
+    type: "business"
+  },
+  {
+    id: 9,
+    name: "Matheus",
+    document: "49818286839",
+    type: "individual"
+  },
+  {
+    id: 10,
+    name: "Matheus",
+    document: "37399356000150",
+    type: "business"
+  },
+];
+
+export default users;
